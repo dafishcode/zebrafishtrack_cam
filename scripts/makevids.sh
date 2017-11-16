11f3aeaedcc5673751c0fcf98d1b5e6142a39bb9
giovannidiana <giovanni.diana@kcl.ac.uk>
2017-11-16 20:41:13 +0000

    Moved Vids one dir up to root of experiemnt

--- scripts/makevids.sh
+++ scripts/makevids.sh
@@ -1,5 +1,7 @@
 echo ".BATCH Convert to MP4 from Pgm. 2017 kostasl"
 echo "Make HD Vids from Image Sequence Dirs.."
+
+fps=$2
 for dir in `find $1 -type d`
 do
   #test -d "$dir" || continue
@@ -11,7 +13,7 @@ do
 	  filename=${dir//[\/]/_}
 	  filename=${filename//[.]/}
 	  echo $filename
-	   avconv -framerate 300 -i $dir/%10d.pgm -c:v libx264 -crf 17 -crf_max 45 $dir/../$filename.mp4
+	   avconv -framerate $fps -i $dir/%10d.pgm -c:v libx264 -crf 17 -crf_max 45 $dir/../../$filename.mp4
 	fi
 
 
